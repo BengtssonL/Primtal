9a75b43e292d954cd1df2a32c556b4819708ec38
Lucas Bengtsson <lucas.bengtsson77@gmail.com>
2023-03-15 15:04:49 +0100

    Add project files.

---
+++ matteg/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matteg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv m ");
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine("skriv n ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> perfectNumbers = new List<int>();
+            for (int i = m; i <= n; i++)
+            {
+                if (IsPerfectNumber(i))
+                {
+                    perfectNumbers.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", perfectNumbers));
+            Console.ReadLine();
+
+        }
+
+        static bool IsPerfectNumber(int number)
+        {
+            int sum = 1;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    sum += i;
+                    if (i != number / i)
+                    {
+                        sum += number / i;
+                    }
+                }
+            }
+            return sum == number && number > 1;
+        }
+    }
+}
